42b3ad2bc14ccc6b61306643aa2dd5ae44373df6
Mateusz Dudzinski <mateuszpd7@gmail.com>
2018-04-17 00:02:42 +0200

    Removed ttf fonts for now, (Will use X fixed font for now).
    Removed stb_truetype header file.

--- main.c
+++ main.c
@@ -11,89 +11,12 @@
 #include <X11/Xutil.h>
 
 #include <assert.h> // I include this to test return values the lazy way
-#include <unistd.h> // So we got the profile for 10 seconds
+#include <unistd.h>
 
 #define WINDOW_W (640)
-#define WINDOW_H (480)
+#define WINDOW_H (125)
 
 int nanosleep(const struct timespec *req, struct timespec *rem);
-
-#define STB_TRUETYPE_IMPLEMENTATION
-#include "stb_truetype.h"
-
-#define TTF_BUFFER_SIZE (1 << 25)
-char unsigned ttf_buffer[TTF_BUFFER_SIZE];
-
-typedef struct
-{
-    unsigned char *pixels;
-    int w, h;
-} BitmapInfo;
-BitmapInfo characters['z'-'a'+1][2];
-
-// TODO: testcode:
-static unsigned char alien_bits[] =
-{
-    0x13, 0xc8, 0xa2, 0x45, 0xc3, 0xc3, 0xe2, 0x47, 0xf3, 0xcf, 0xbe, 0x7d,
-    0xf8, 0x1f, 0xf8, 0x1f, 0xf8, 0x1f, 0x3f, 0xfc, 0xf2, 0x4f, 0xe3, 0xc7,
-    0xc2, 0x43, 0x83, 0xc1, 0x02, 0x40, 0x03, 0xc0
-};
-
-
-static BitmapInfo a; // TODO: Dont make itt global, fix once cleanup.
-
-static void
-stb_test()
-{
-    stbtt_fontinfo font;
-    int w, h, i, j, fsize = 256;
-
-    fread(ttf_buffer, 1, TTF_BUFFER_SIZE,
-          fopen("/usr/share/fonts/TTF/DejaVuSans.ttf", "rb"));
-
-    stbtt_InitFont(&font, ttf_buffer,
-                   stbtt_GetFontOffsetForIndex(ttf_buffer, 0));
-
-    for (int caps = 0; caps < 2; ++caps)
-        for (int letter = 'a'; letter <= 'z'; ++letter)
-        {
-            unsigned char *bitmap = stbtt_GetCodepointBitmap(
-                &font, 0, stbtt_ScaleForPixelHeight(&font, fsize),
-                (caps ? letter + 'A' - 'a' : letter), &w, &h, 0,0);
-
-            characters[letter - 'a'][caps] = (BitmapInfo){ bitmap, w, h };
-        }
-
-    for (int caps = 0; caps < 2; ++caps)
-        for (int letter = 0; letter < 'z' - 'a' + 1; ++letter)
-        {
-            putchar('\n');
-            putchar('\n');
-            BitmapInfo bi = characters[letter][caps];
-            for (j = 0; j < bi.h; ++j)
-            {
-                for (i =0 ; i < bi.w; ++i)
-                    putchar(" .:ioVM@"[bi.pixels[j* bi.w +i]>>5]);
-                putchar('\n');
-            }
-        }
-}
-
-unsigned char temp_bitmap[1 << 20];
-stbtt_bakedchar cdata[96]; // ASCII 32..126 is 95 glyphs
-
-static void
-stb_test2()
-{
-    fread(ttf_buffer, 1, 1 << 20,
-          fopen("/usr/share/fonts/TTF/DejaVuSans.ttf", "rb"));
-
-    int bake_font_bitmap_res;
-    bake_font_bitmap_res = stbtt_BakeFontBitmap(ttf_buffer, 0, 50, temp_bitmap,
-                                                1024, 1024, 'a', 'z'-'a'+1, cdata);
-
-    printf("Baking bitmap result: %d\n", bake_font_bitmap_res);
-}
 
 Display *dpy;
 Window w;
@@ -102,11 +25,15 @@ XFontStruct *font;
 GC gc;
 int whiteColor, blackColor;
 Pixmap pixmap;
+Colormap color_map;
+XColor bg_color[2];
+
+const int bg_red = 0x1e, bg_green = 0x1f, bg_blue = 0x1c;
 
 static void
 set_up_font ()
 {
-    const char * fontname = "-*-helvetica-*-r-*-*-18-*-*-*-*-*-*-*";
+    const char * fontname = "-*-fixed-*-*-*-*-20-*-*-*-*-*-*-*"; // "-*-helvetica-*-r-*-*-18-*-*-*-*-*-*-*";
     font = XLoadQueryFont (dpy, fontname);
 
     /* If the font could not be loaded, revert to the "fixed" font. */
@@ -120,14 +47,38 @@ set_up_font ()
 char inserted_text[256];
 int inserted_text_idx = 0;
 
-#define NUMBER_OF_ENTRIES (5)
+#define NUMBER_OF_ENTRIES (25)
 const char *entries[NUMBER_OF_ENTRIES] =
 {
     "Foo",
     "Bar",
     "FooBar",
     "Scoo",
-    "Test"
+    "Test",
+
+    "Another string",
+    "More text",
+    "Even more text",
+    "Options options options",
+    "More options",
+
+    "Text, text, text",
+    "Trololololo ",
+    "Blah, Blah, Blah, Blah, Blah, Blah, ",
+    "Teeeeeext",
+    "This is even more stupid text",
+
+    "Another string",
+    "More text",
+    "Even more text",
+    "Options options options",
+    "More options",
+
+    "Foo again",
+    "Bar again",
+    "FooBar again",
+    "Scoo again",
+    "Test again"
 };
 
 static int
@@ -155,13 +106,16 @@ getLettersCount(const char *text)
 static void
 redrawWindow()
 {
-    // Draw the line
-    // XDrawLine(dpy, w, gc, 10, 60, 180, 20);
-    // XDrawRectangle(dpy, w, gc, 0, 0, 200-1, 100-1);
-    // TODO: Dont draw the background all the time.
-    XSetForeground(dpy, gc, blackColor);
+    // Draw the background.
+    XSetForeground(dpy, gc, bg_color[0].pixel);
     XFillRectangle(dpy, w, gc, 0, 0, WINDOW_W, WINDOW_H);
+
+    // Draw the margins:
     XSetForeground(dpy, gc, whiteColor);
+    XFillRectangle(dpy, w, gc, 0, 0, WINDOW_W, 1);
+    XFillRectangle(dpy, w, gc, 0, 0, 1, WINDOW_H);
+    XFillRectangle(dpy, w, gc, WINDOW_W-1, 0, WINDOW_W, WINDOW_H);
+    XFillRectangle(dpy, w, gc, 0, WINDOW_H-1, WINDOW_W, WINDOW_H);
 
     int x = 0;
     int y = 0;
@@ -170,7 +124,6 @@ redrawWindow()
     int descent = 0;
     XCharStruct overall;
 
-    /* Centre the text in the middle of the box. */
     XTextExtents (font, "Hello World!", getLettersCount("Hello World!"),
                   &direction, &ascent, &descent, &overall);
 
@@ -192,33 +145,7 @@ redrawWindow()
             displayed_entries++;
         }
     }
-    // TODO: Super-creepy so far! I would rather not use it, because it is
-    // more likely to break something than work... For now.
-#if 1
-
-    XSetBackground(dpy, gc, whiteColor);
-    XSetForeground(dpy, gc, blackColor);
-
-    // NOTE: USe one of these two??
-    // XCopyArea(dpy, pixmap, w, gc, 0, 0,
-    //          a.w, a.h, 100, 50);
-
-    /* XCopyPlane(dpy, pixmap, w, gc, 0, 0, */
-    /* a.w, a.h, a.w, a.h, 1); */
-
-    // TODO: Copied from dmenu.
-    // NOTE: Copied Area seeps not to appear so we give it some time.
-
-#if 0
-    struct timespec tim;
-    tim.tv_sec = 0;
-    tim.tv_nsec = 1000 * 1000 * 10;
-    nanosleep(&tim, NULL);
-#endif
-
-#endif
     XFlush(dpy);
-
 
 #if 0
     printf(inserted_text);
@@ -229,48 +156,19 @@ redrawWindow()
 int
 main()
 {
-    // stb test
-    stb_test2();
-
-    for (int i = 0; i < 96; ++i)
-    {
-        stbtt_bakedchar character_info = cdata[i];
-        printf("%d: (%d;%d)x(%d;%d), %f, %f, %f\n", i,
-               character_info.x0, character_info.y0,
-               character_info.x1, character_info.y1,
-               character_info.xoff, character_info.yoff,
-               character_info.xadvance);
-    }
-
-    // (29;1)x(41;17)
-    // (1;1)x(14;17)
-    int x0, y0, x1, y1;
-
-#if 1
-    stbtt_bakedchar c = cdata[3];
-    x0 = c.x0; y0 = c.y0;
-    x1 = c.x1; y1 = c.y1;
-#else
-    x0 = 1; y0 = 1;
-    x1 = 14; y1 = 17;
-#endif
-
-    for (int j = y0; j < y1; ++j)
-    {
-        for (int i = x0; i < x1; ++i)
-            putchar(" .:ioVM@"[temp_bitmap[j * 1024 + i] >> 5]);
-            /* printf("%d ", temp_bitmap[j * 1024 + i]); */
-        putchar('\n');
-    }
-    //endof stb test.
-
     inserted_text[0] = '\0';
 
     dpy = XOpenDisplay(NULL);
     assert(dpy);
 
+    // Initialize color map.
+    color_map = XDefaultColormap(dpy, 0);
     blackColor = BlackPixel(dpy, DefaultScreen(dpy));
     whiteColor = WhitePixel(dpy, DefaultScreen(dpy));
+
+    bg_color[0].red = bg_red * 256; bg_color[0].green = bg_green * 256; bg_color[0].blue = bg_blue * 256;
+    bg_color[0].flags = DoRed | DoGreen | DoBlue;
+    XAllocColor(dpy, color_map, bg_color);
 
     // MY:
     XSetWindowAttributes wa;
@@ -316,7 +214,11 @@ main()
         exit(3);
     }
 
-//   XUngrabKeyboard(dpy, CurrentTime);
+
+    // For testing stuff that does not work with keyboard nor needs it.
+#if 0
+    XUngrabKeyboard(dpy, CurrentTime);
+#endif
 
     XChangeWindowAttributes(dpy, w, 0, &wa);
 
@@ -336,42 +238,36 @@ main()
     XSetBackground(dpy, gc, whiteColor);
 #endif
 
+    // TODO: DOnt need them here:
     XSetForeground(dpy, gc, blackColor);
+
+#if 1
+    // I guess XParseColor will work here
+    XSetForeground(dpy, gc, bg_color[0].pixel);
+#endif
     XFillRectangle(dpy, w, gc, 0, 0, WINDOW_W, WINDOW_H);
 
     set_up_font ();
 
+    XFlush(dpy);
+
 #if 1
     // a = characters[0][1];
 
-    XSetForeground(dpy, gc, whiteColor);
     // TODO: PRobobly not the best way, but who cares?
-    int depth = DefaultDepth(dpy, DefaultScreen(dpy));
+    // int depth = DefaultDepth(dpy, DefaultScreen(dpy));
     /* pixmap = XCreatePixmapFromBitmapData(dpy, w, (char *)a.pixels, a.w, a.h, */
     /* whiteColor, blackColor, depth); */
 
-    stbtt_bakedchar character_info = cdata[0];
-    pixmap = XCreatePixmap(dpy, w,
-                           character_info.y1-character_info.y0,
-                           character_info.x1-character_info.x0,
-                           depth);
-
-    // TODO: set pixels of the bitmap!
-
-    XCopyArea(dpy, pixmap,  w, gc, 0, 0,
-              character_info.y1-character_info.y0,
-              character_info.x1-character_info.x0,
-              40, 40);
-
-    XFlush(dpy);
-
-    sleep(2);
+    // XFlush(dpy);
 
     /* XCreatePixmap(dpy, DefaultRootWindow(dpy), a.w, a.h, depth); */
     /* for (int i = 0; i < a.w; ++i) */
     /* for (int j = 0; j < a.h; ++j) */
     /* XDrawPoint(dpy, pixmap, gc, i, j); */
 #endif
+
+    redrawWindow();
 
     // Wait for the MapNotify event
     while(1)
@@ -412,6 +308,7 @@ main()
                 case XK_Return:
                 {
                     printf(inserted_text);
+                    XUngrabKeyboard(dpy, CurrentTime);
                     exit(0);
                 } break;
 
