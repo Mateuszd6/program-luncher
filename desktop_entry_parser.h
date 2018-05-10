2e068a9ae0e0e9090fcd58121bc244e8cac9c119
Mateusz Dudzinski <mateuszpd7@gmail.com>
2018-05-11 01:08:53 +0200

    Missed files from the previous commit.

---
+++ desktop_entry_parser.h
@@ -0,0 +1,10 @@
+#ifndef __DESKTOP_ENTRY_PARSER_H__
+#define __DESKTOP_ENTRY_PARSER_H__
+
+typedef struct
+{
+    char *name;
+    char *exec;
+} DesktopEntry;
+
+#endif // __DESKTOP_ENTRY_PARSER_H__
---
+++ string.h
@@ -0,0 +1,20 @@
+#ifndef __STRING_H__
+#define __STRING_H__
+
+// If the entry is less than this characters long, it will be stored in a
+// struct, not by the pointer, and the access will be faster.
+#ifndef STRING_CHARACTERS_IN_SHORT_TEXT
+#define STRING_CHARACTERS_IN_SHORT_TEXT (56)
+#endif
+
+typedef struct
+{
+    char text[STRING_CHARACTERS_IN_SHORT_TEXT];
+    char *large_text;
+} String;
+
+static inline char *StringGetText(String *str);
+
+static inline String MakeStringCopyText(const char *text, int text_len);
+
+#endif // __STRING_H__
